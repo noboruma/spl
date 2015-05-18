c9ee11defe2e6496e29dc33c35f554f2e77e5c98
Thomas Legris <legris.thomas@gmail.com>
2015-05-18 05:35:27 -0400

    Add Upsampler impl

--- tests/check_histogram.cc
+++ tests/check_histogram.cc
@@ -20,7 +20,7 @@ int main()
   for_each_pixels(test, x, y)
     test(x,y) = arr_test[y][x];
 
-  spl::Histogram<spl::Signal2D<value_type>, 9, 0 > h(test);
+  spl::Histogram<spl::Signal2D<value_type>> h(test);
 
   h();
 
