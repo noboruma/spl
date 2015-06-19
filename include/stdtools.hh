c22e22a58393e1c47f60b3551cd4f673b818a4c9
Thomas Legris <legris.thomas@gmail.com>
2015-06-19 05:56:21 -0400

    Add approximation equality

--- include/stdtools.hh
+++ include/stdtools.hh
@@ -9,6 +9,7 @@
 #include <iomanip>
 #include <type_traits>
 #include <limits>
+#include <cmath>
 
 /* **
  * This file is juste a set of convenient standard methods from std11 such
@@ -20,6 +21,20 @@
 
 namespace std
 {
+  template<bool exclude=true, typename T>
+  bool approx_equal(T a, T b, T epsilon=std::numeric_limits<T>::epsilon())
+  {
+    if(std::numeric_limits<T>::is_integer)
+      if(exclude)
+        return std::abs(a-b) < epsilon;
+      else
+        return std::abs(a-b) <= epsilon;
+    else
+      if(exclude)
+        return std::fabs(a-b) < epsilon;
+      else
+        return std::fabs(a-b) <= epsilon;
+  }
 
   template< typename T >
   inline T convert(const std::string& str)
