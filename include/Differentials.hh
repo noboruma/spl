8de7d07123ede59dd0ee43ea893c691129d6754c
Aymeric Reshef <you@example.com>
2015-07-15 19:08:00 +0000

    Add divergence operators

--- include/Differentials.hh
+++ include/Differentials.hh
@@ -4,21 +4,27 @@
   namespace spl{
     namespace differentials 
     {
+      //enum numeric { NUM_FORWARD, NUM_CENTERED } ;
       // Gradient (local operator -- one orientation)
       template <typename E>
       traits_value_type(E) gradient(const E &sig,
                                     const traits_point_type(E) &p,
-                                    unsigned i)
+                                    const unsigned i)
       {
-        traits_point_type(E) q ;
-        for (unsigned j = 0 ; j < traits_domain_dim(E) ; j++)
-        {
-          if (j==i)
+        //switch (num)
+        //{
+        //  case NUM_FORWARD:
+        //  {
+            traits_point_type(E) q(p) ;
             q[i] = ( p[i] == sig.domain()[i]-1 ) ? p[i] : p[i]+1 ;
-          else
-            q[j] = p[j] ;
-        }
-        return sig[q] - sig[p] ;
+            return sig[q] - sig[p] ;
+        //  }
+        //  case NUM_CENTERED:
+        //  {
+        //    traits_point_type(E) q(p), r() ;
+        //    q[i] = 
+        //  }
+        //}
       }
 
       // Gradient (local operator -- all orientations)
@@ -49,19 +55,55 @@
       {
         Signal1D<E> grad(traits_domain_dim(E), sig.domain());
         for_each_lin(grad,i)
-          grad(i) = gradient(sig,i);
+        {
+          std::cout << "life" << std::endl ;
+          const E& tmp = gradient<E>(sig,i);
+          std::cout << tmp(15,15) << std::endl ;
+          for_each_pixel(grad(i),x,y)
+          grad(i)(x,y) = 0;
+        }
         return grad;
       }
 
-//      template <typename E>
-//      NDSignal<Signal1D<traits_value_type(E)>> gradient(E sig)
-//      {
-//        NDSignal<Signal1D<traits_value_type(E)>> grad(sig.domain()) ;
-//        traits_iterator_type(NDSignal<traits_value_type(E)>) it (sig.domain()) ;
-//        for_each_element(it)
-//          grad[it] = spl::differentials::gradient(sig,it) ;
-//        return grad ;
-//      }
+      // Divergence (local operator -- one orientation)
+      template <typename E>
+      traits_value_type(E) divergence(const E &sig,
+                                      const traits_point_type(E) &p,
+                                      unsigned i)
+      {
+        traits_point_type(E) q(p) ;
+        q[i] = ( p[i] == 0 ) ? p[i] : p[i]-1 ;
+        return sig[p] - sig[q] ;
+      }
+
+      // Divergence (local operator -- all orientations)
+      template <typename E>
+      traits_value_type(E) divergence(const Signal1D<E> &sig,
+                                      const traits_point_type(E) &p)
+      {
+        traits_value_type(E) div = 0 ;
+        for_each_lin(sig,i)
+          div += divergence(sig(i),p,i) ;
+        return div ;
+      }
+
+      // Divergence (global operator -- all orientations)
+      template <typename E>
+      E divergence(const Signal1D<E> &sig)
+      {
+        E out(sig(0).domain()) ;
+        traits_iterator_type(E) it(out.domain()) ;
+        for_each_element(it)
+          out[it] = divergence(sig,it) ;
+        return out ;
+      }
+
+      // Laplacian
+      template <typename E>
+      E laplacian(const E &sig)
+      {
+        return divergence(gradient(sig)) ;
+      }
 
     } //!differentials
   }//!spl
